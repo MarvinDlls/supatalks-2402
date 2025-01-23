65bf7e813c753ac26a2f7c3ab32abfa55f82aafa
Marvindlls <marvindillenseger@gmail.com>
2025-01-23 10:15:35 +0100

    ✨ ajouter(HomeController.php, documentation.html.twig, changelog.html.twig) : ajouter des routes pour la documentation et le changelog, et mettre à jour le footer avec des liens appropriés

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home', methods: ['Get'])]
     public function index(
         EventRepository $eventRepository,
     ): Response
@@ -18,6 +18,22 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'title' => 'Supatalks',
             'events' => $eventRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/documentation', name: 'app_documentation', methods: ['GET'])]
+    public function documentation(): Response
+    {
+        return $this->render('page/documentation.html.twig', [
+            'controller_name' => 'documentation',
+        ]);
+    }
+
+    #[Route('/changelog', name: 'app_changelog', methods: ['GET'])]
+    public function cgu(): Response
+    {
+        return $this->render('page/changelog.html.twig', [
+            'controller_name' => 'changelog',
         ]);
     }
 }
